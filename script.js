968f3446bb409849e019d3b2a5da175ac87ac32d
roypunga <royseba2005@gmail.com>
2024-09-17 19:23:02 -0300

    Change logic and add functionality for all buttons

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 
-const button0 = document.querySelector('.zero')
-const button1 = document.querySelector('.one')
-const button2 = document.querySelector('.two')
-const button3 = document.querySelector('.three')
-const button4 = document.querySelector('.four')
-const button5 = document.querySelector('.five')
-const button6 = document.querySelector('.six')
-const button7 = document.querySelector('.seven')
-const button8 = document.querySelector('.eight')
-const button9 = document.querySelector('.nine')
-
-const buttonPlus = document.querySelector('button.plus')
-const buttonMinus = document.querySelector('button.minus')
-const buttonDivide = document.querySelector('button.divide')
-const buttonMultiply = document.querySelector('button.multiply')
 const buttonEquals = document.querySelector('button.equals')
-const buttonPoint = document.querySelector('button.point')
 const buttonBackspace = document.querySelector('button.backspace')
 const buttonPositiveNegative = document.querySelector('button.positiveNegative')
 const buttonAc = document.querySelector('button.ac')
 
+const inputDiv = document.querySelector('div.input')
+const operationDiv = document.querySelector('div.operation')
+
 const opButtons = document.querySelectorAll('.op')
 const numButtons = document.querySelectorAll('.num')
+
+let input = '';
+let num1 = 0;
+let num2;
+let operator;
+let lastButtonPressedIsEquals
+
 
 function operate(n1, operator, n2){
     switch(operator){
@@ -45,19 +39,38 @@ function operate(n1, operator, n2){
         }
 }
 
-let input = '';
-let num1 = 0;
-let num2;
-let operator;
 
+buttonAc.addEventListener('click', ()=>{
 
+ window.location.reload();
+
+})
+
+buttonBackspace.addEventListener('click', ()=>{
+
+    input = input.substring(0, input.length - 1);
+    inputDiv.textContent = input
+
+})
+
+buttonPositiveNegative.addEventListener('click', ()=>{
+    input = input + '-'
+    inputDiv.textContent = input
+})
 
 numButtons.forEach((num) => {
 
     num.addEventListener('click', ()=>{
 
+        if(lastButtonPressedIsEquals){
+            lastButtonPressedIsEquals = false
+            num2 = null
+            num1 = 0
+        }
+
         input = input + num.innerHTML
-            
+        
+        inputDiv.textContent = input
     })
 
 })
@@ -68,17 +81,77 @@ opButtons.forEach((op) =>{
 
     op.addEventListener('click', ()=>{
 
-        operator = op.innerHTML
-
-        result = operate(num2, operator, num1)
-
-        num2 = num1;
-
-        num1 = input * 1
         
+        if(num2 == null){
+            operator = op.innerHTML
+            num2 = num1
+            num1 = input * 1
+            operationDiv.textContent = num1 + operator
+            
+            input = ''
+            inputDiv.textContent = input
+        }
+        else if(!lastButtonPressedIsEquals){
 
-        input = ''
+            num2 = num1
+            num1 = input *1
+            lastButtonPressedIsEquals = false
+
+
+            
+            if(operate(num2, operator, num1) == 'div0'){
+                
+                inputDiv.textContent = 'NOOOOOOOOO'
+                operationDiv.textContent = ''
+            }
+            else{
+                num1 = operate(num2, operator, num1)
+                operator = op.innerHTML
+                operationDiv.textContent = num1 + operator
+                
+                
+                input = ''
+                inputDiv.textContent = input
+            }
+        }
+        else{
+            lastButtonPressedIsEquals = false
+            operator = op.innerHTML
+            if(operate(num2, operator, num1) == 'div0'){
+                
+                inputDiv.textContent = 'NOOOOOOOOO'
+                operationDiv.textContent = ''
+            }
+            else{
+                operationDiv.textContent = num1 + operator
+                input = ''
+                inputDiv.textContent = input
+            }
+        }
 
     })
 
+})
+
+
+
+
+buttonEquals.addEventListener('click', ()=>{
+    num2 = num1;
+    num1 = input * 1
+    
+    if(operate(num2, operator, num1) == 'div0'){
+        inputDiv.textContent = 'NOOOOOOOOO'
+        operationDiv.textContent = ''
+        num1 = 0
+        num2 = null
+        input = ''
+    }
+    else{
+        num1 = operate(num2, operator, num1)
+        inputDiv.textContent = num1
+        input = ''
+        operationDiv.textContent = ''
+        lastButtonPressedIsEquals = true
+    }
 })
